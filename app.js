0a968cc39bcbef35ea61ebc1264758ed131457c7
Gabriel Bercelline <Gabrielbercelline@gmail.com>
2021-06-30 16:56:15 -0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://bercelline:bertolli@cluster0.c89rt.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://bercelline:bertolli@cluster0.c89rt.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true})
 .then( () => {
   console.log('Acessar http://localhost:3000');
